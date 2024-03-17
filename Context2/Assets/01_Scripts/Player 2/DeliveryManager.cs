aeacb829953033cf7d7e0b3c9a5b07588607d660
GooForSupper <llvandersloot@gmail.com>
2024-03-17 17:18:30 +0100

    Added Ranking UI

--- Context2/Assets/01_Scripts/Player 2/DeliveryManager.cs
+++ Context2/Assets/01_Scripts/Player 2/DeliveryManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DeliveryManager : MonoBehaviour
 {
@@ -15,23 +16,34 @@ public class DeliveryManager : MonoBehaviour
     [SerializeField] float sRankTime = 30;
     [SerializeField] float aRankTime = 45;
     [SerializeField] float bRankTime = 60;
-    [SerializeField] float cRankTime = 75;
     [SerializeField] TextMeshProUGUI timerText;
+    [Space]
+    [SerializeField] Slider timeBar;
 
     [Space]
+    [Header("UI")]
+    [SerializeField] GameObject sRankPopup;
+    [SerializeField] GameObject aRankPopup;
+    [SerializeField] GameObject bRankPopup;
+    [SerializeField] GameObject cRankPopup;
     [SerializeField] TextMeshProUGUI deliveriesMadeText;
     [SerializeField] TextMeshProUGUI rankInfoText;
+
+    Animator timerAnimator;
+    float second = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         deliveriesMadeText.text = "Deliveries made: " + deliveriesMade.ToString();
+        timerAnimator = timerText.GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         IncreaseTime();
+        TimeSlider();
 
         timerText.text = passedTime.ToString("F2");
 
@@ -52,15 +64,29 @@ public class DeliveryManager : MonoBehaviour
             rankInfoText.text = currentRank.ToString() + " Rank";
         }
 
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            Time.timeScale = 3;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
     }
 
     public void Delivered()
     {
-
+        // Display Aquired Rank
         Debug.Log("Rank " + currentRank + "! ~ Delievered in " + passedTime + " seconds.");
+        if (currentRank == Ranks.S) { sRankPopup.SetActive(true); }
+        else if (currentRank == Ranks.A) { aRankPopup.SetActive(true); }
+        else if (currentRank == Ranks.B) { bRankPopup.SetActive(true); }
+        else if (currentRank == Ranks.C) { cRankPopup.SetActive(true); }
 
         // Reset time
         passedTime = 0;
+        second = 0;
 
         // Increase delivery counter
         deliveriesMade++;
@@ -71,11 +97,37 @@ public class DeliveryManager : MonoBehaviour
     {
         passedTime += Time.deltaTime;
 
+        second += Time.deltaTime;
+        if (second >= 1) { second = 0; timerAnimator.SetTrigger("shake"); }
+
         // Check deserved rank based on time + set enum to this rank
         if (passedTime < sRankTime) { currentRank = Ranks.S; }
         else if (passedTime < aRankTime) { currentRank = Ranks.A; }
         else if (passedTime < bRankTime) { currentRank = Ranks.B; }
-        else if (passedTime < cRankTime) { currentRank = Ranks.C; }
+        else if (passedTime > bRankTime) { currentRank = Ranks.C; }
+    }
+
+    void TimeSlider()
+    {
+        if (passedTime <= sRankTime)
+        {
+            timeBar.maxValue = sRankTime;
+            timeBar.value = passedTime;
+        }
+        else if (passedTime <= aRankTime)
+        {
+            timeBar.maxValue = aRankTime - sRankTime;
+            timeBar.value = passedTime - sRankTime;
+        }
+        else if (passedTime <= bRankTime)
+        {
+            timeBar.maxValue = bRankTime - aRankTime;
+            timeBar.value = passedTime - aRankTime;
+        }
+        else if (passedTime > bRankTime)
+        {
+            timeBar.value = 0;
+        }
     }
 
     IEnumerator ShowRank()
