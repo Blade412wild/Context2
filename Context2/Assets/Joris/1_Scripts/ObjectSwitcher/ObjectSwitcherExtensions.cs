f0d2b2bbadc012569312ac44a458b1dbeb4d7847
TheDutchKnot <Joris.Plagge@gmail.com>
2024-03-21 15:37:57 +0100

    NU niet meer rood

---
+++ Context2/Assets/Joris/1_Scripts/ObjectSwitcher/ObjectSwitcherExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public static class ObjectSwitcherExtensions
+{
+    public static bool HasPath(this NavMeshAgent agent, Vector3 targetPos)
+    {
+        var path = new NavMeshPath();
+
+        if (agent.CalculatePath(targetPos, path) && path.status == NavMeshPathStatus.PathComplete)
+            return true;
+
+        return false;
+    }
+
+    public static T RandomItem<T>(this T[] array)
+    {
+        if (array.Length == 0)
+            throw new System.IndexOutOfRangeException("Cannot select a random item from an empty array");
+
+        return array[UnityEngine.Random.Range(0, array.Length)];
+    }
+
+    public static T GetOrAdd<T>(this GameObject gameObject) where T : Component
+    {
+        T component = gameObject.GetComponent<T>();
+        if (!component) component = gameObject.AddComponent<T>();
+
+        return component;
+    }
+}
