dccf1b1d39015fe5af94820f2ba7b98e311e9bcb
TheDutchKnot <Joris.Plagge@gmail.com>
2024-03-21 15:26:32 +0100

    Added DestinationManager
    gl lucas

---
+++ Context2/Assets/Joris/1_Scripts/DestinationSystem/DestinationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// Summary: 
+//      Attach to Player!
+//      
+//      Calculates new Destinations based on navmesh pathfinding.
+public class DestinationManager : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _playerAgent;
+    [SerializeField] private List<Transform> _destinationsEditor;
+
+    public Vector3 CurrentDestination { get; private set; }
+
+    private Transform[] _destinations;
+
+    private void Awake()
+    {
+        gameObject.GetOrAdd<NavMeshAgent>();
+        _destinations = _destinationsEditor.ToArray();
+    }
+
+    private void NextDestination()
+    {
+        Vector3 newPos = _destinations.RandomItem().position;
+
+        if (_playerAgent.HasPath(newPos))
+            SetDestination(newPos);
+        else
+            NextDestination();
+    }
+
+    private void SetDestination(Vector3 pos)
+    {
+        CurrentDestination = pos;
+
+        // Whatever the fuck is supposed to be happening here!!
+    }
+}
