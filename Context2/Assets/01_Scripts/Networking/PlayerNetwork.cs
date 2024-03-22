0f362e5fc743a31c7777b4628bb90753a99096a6
Blade412wild <nathanhoh951@gmail.com>
2024-03-22 21:22:41 +0100

    Spawning Players Works

--- Context2/Assets/01_Scripts/Networking/PlayerNetwork.cs
+++ Context2/Assets/01_Scripts/Networking/PlayerNetwork.cs
@@ -21,23 +21,10 @@ public class PlayerNetwork : NetworkBehaviour
     {
         if (!IsOwner) return;
 
-        if (Input.GetKeyDown(KeyCode.B) && OwnerClientId != 0)
+        if (Input.GetKeyDown(KeyCode.B) && OwnerClientId == 1)
         {
             TestServerRpc();
         }
-
-        TestServerRpc();
-        Vector3 moveDir = new Vector3(0, 0, 0);
-
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
-
-
-        float moveSpeed = 3f;
-        transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
-
     }
 
     [ServerRpc]
